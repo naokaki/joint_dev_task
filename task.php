785184a7688949d056971300513580e93c20a683
naokaki <naokaki0228@softbank.ne.jp>
2021-03-09 05:32:34 +0900

    PHP課題（基礎）Q6までの提出

--- task.php
+++ task.php
@@ -1,11 +1,12 @@
+<?php
 # 課題の回答は このファイル をご利用下さい。
 # 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
 
 print("#####q1#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
-
   # 以下に回答を記載
-
+array_push($names,"斎藤");
+print_r($names);
 echo PHP_EOL;
 
 print("#####q2#####".PHP_EOL);
@@ -13,43 +14,61 @@ $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
-
+$array3 = array_merge($array1, $array2);
+print_r($array3);
 echo PHP_EOL;
 
 print("#####q3#####".PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
-
+$three_count = 0;
+foreach ($numbers as $number) {
+  if ($number == 3) {
+    $three_count++;
+  }
+}
+echo "3は".$three_count."個あります。";
 echo PHP_EOL;
 
 print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
-
+foreach ($sports as $key => $value) {
+  if (is_null($value)) {
+    unset($sports[$key]);
+ }
+}
+print_r($sports);
 echo PHP_EOL;
+
 
 print("#####q5#####".PHP_EOL);
 $array1 = [];
 $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
-
+var_export(empty($array1));
+echo PHP_EOL;
+var_export(empty($array2));
 echo PHP_EOL;
 
 print("#####q6#####".PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
-
+foreach ($numbers1 as $value) {
+  $value *= 10;
+  $numbers2[] = $value;
+}
+print_r($numbers2);
 echo PHP_EOL;
 
 print("#####q7#####".PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
   # 以下に回答を記載
-
   # 以下は変更しないで下さい
 var_dump($array);
 
@@ -205,3 +224,4 @@ foreach($humans as $human){
 }
 
 echo PHP_EOL;
+ ?>
